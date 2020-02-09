e2580df70504e9acc5f25d56ca757f877c36b3dc
Nyannazz <kantemir.imam@gmail.com>
2020-02-09 12:55:35 +0100

    fancy upload preview added

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -6,18 +6,29 @@
         <div class="inner test">
             <form id="uploadForm" action="/post" method="POST" enctype="multipart/form-data" class="flexCenterAllVert">
                 <input accept="image/" type="file" name="image" id="file">
-                <label for="file" class="centerAll" id="dropZone">
-                    <strong class="pointer">Choose a file &nbsp</strong>
-                    <span>or drag and dropt it here</span>
-                    <h3>DROP YOUR FILE! :)</h3>
-                </label>
+                
+                <div id="dropZoneWrapper"  class="centerAll">
+                    <div id="uploadInfo">
+                        <img class="margin0Auto" src="{{asset('/icons/square-upload.svg')}}" alt="upload"/>
+                        <strong class="pointer">Choose a file &nbsp</strong>
+                        <span>or drag and dropt it here</span>
+                        <h3>DROP YOUR FILE! :)</h3>
+                    </div>
+                    
+
+                    <label for="file" id="dropZone">
+                        
+                    </label>
+                </div>
+
                 
                 <div id="uploadPreview">
+                    <div id="uploadPreviewClose" class="closeIcon centerAll pointer">X</div>
                     <img id="uploadPreviewImage" src="" alt="image preview">
                 </div>
 
-                <input type="text" name="title">
-                <input type="submit">
+                <input class="textInput" type="text" name="title">
+                <input class="mainSubmit" type="submit">
                 @csrf
             </form>
         </div>
@@ -25,7 +36,10 @@
     <script>
         const input=document.querySelector("#file");
         const dropZone=document.querySelector("#dropZone");
+        const dropZoneWrapper=document.querySelector("#dropZoneWrapper");
         const uploadForm=document.querySelector('#uploadForm')
+
+        document.querySelector('#uploadPreviewClose').addEventListener('click', resetUpload)
 
         dropZone.addEventListener("dragover", handleDragEnter, false)
         dropZone.addEventListener("dragenter", handleDragEnter, false)
@@ -38,28 +52,47 @@
         function handleDragEnter(event){
             event.preventDefault();
             event.stopPropagation();
-            //if(event.currentTarget!==event.target) return
-            dropZone.classList.add("dragOver")
+            dropZoneWrapper.classList.add("dragOver")
         }
         function handleDragLeave(event){
             event.preventDefault();
             event.stopPropagation();
             if(event.currentTarget!==event.target) return
-            dropZone.classList.remove("dragOver")
+            dropZoneWrapper.classList.remove("dragOver")
         }
 
         function handleDrop(event){
             event.preventDefault();
             event.stopPropagation();
             if(event.currentTarget!==event.target) return
-            dropZone.classList.remove("dragOver")
+            dropZoneWrapper.classList.remove("dragOver")
             const dataTransfer=event.dataTransfer;
             const files=dataTransfer.files;
             if(files.length>1) return alert("only single file upload allowed!")
             
             input.files=files
 
-            uploadForm.classList.add("showUploadPreview");
+            showPreview(input)
+
+        }
+        function showPreview(input){
+            if(input.files && input.files[0]){
+                const reader=new FileReader();
+
+                reader.onload=function(event){
+                    const uploadPreviewImage=document.querySelector("#uploadPreviewImage");
+                    uploadPreviewImage.src=event.target.result;
+
+                    /* once filedata is loaded inside the browser hidde the dropZone and show the image instead */
+                    uploadForm.classList.add("showUploadPreview");
+                }
+                reader.readAsDataURL(input.files[0])
+
+            }
+        }
+        function resetUpload(){
+            uploadForm.classList.remove("showUploadPreview");
+            uploadForm.reset();
         }
 
     </script>  
